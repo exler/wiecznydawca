fa0c8a38a98b6849a3fee138050027dd9c945abd
Kamil Marut <kamil@kamilmarut.com>
2022-11-02 14:06:44 +0100

    feat(client): Improve navbar on mobile

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -10,26 +10,18 @@ export default function Navbar() {
     const user = useUser();
 
     const renderLinks = () => {
-        if (!user) {
-            return (
-                <>
-                    <li><Link href="/zaloguj">Zaloguj się</Link></li>
-                </>
-            )
-        } else {
-            return (
-                <>
-                    <li className="mr-2"><Link href="/krwiodawca/dodaj" className="lg:btn lg:btn-primary">Nowy wpis</Link></li>
-                    <li className="mr-2"><Link href="/krwiodawca/dziennik">Dziennik</Link></li>
-                    <li className="mr-2"><Link href="/krwiodawca/odznaki">Odznaki</Link></li>
-                    <li className="mr-2"><Link href="/krwiodawca/ustawienia">Ustawienia</Link></li>
-                    <li><button onClick={async () => {
-                        await supabaseClient.auth.signOut();
-                        router.push("/zaloguj");
-                    }}>Wyloguj się</button></li>
-                </>
-            )
-        }
+        return (
+            <>
+                <li className="mr-2"><Link href="/krwiodawca/dodaj" className="lg:btn lg:btn-primary">Nowy wpis</Link></li>
+                <li className="mr-2"><Link href="/krwiodawca/dziennik">Dziennik</Link></li>
+                <li className="mr-2"><Link href="/krwiodawca/odznaki">Odznaki</Link></li>
+                <li className="mr-2"><Link href="/krwiodawca/ustawienia">Ustawienia</Link></li>
+                <li><button onClick={async () => {
+                    await supabaseClient.auth.signOut();
+                    router.push("/zaloguj");
+                }}>Wyloguj się</button></li>
+            </>
+        )
     }
 
     return (
@@ -39,21 +31,31 @@ export default function Navbar() {
                     <Image src={logo} alt="Wieczny Dawca" height={48} />
                 </Link>
             </div>
-            <div className="dropdown">
-                <label tabIndex={0} className="btn btn-ghost lg:hidden">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
-                    </svg>
-                </label>
-                <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box">
-                    {renderLinks()}
-                </ul>
-            </div>
-            <div className="hidden lg:flex">
-                <ul className="menu menu-horizontal p-0">
-                    {renderLinks()}
-                </ul>
-            </div>
+            {!user ? (
+                <div className="flex">
+                    <ul className="menu menu-horizontal p-0">
+                        <li><Link href="/zaloguj">Zaloguj się</Link></li>
+                    </ul>
+                </div>
+            ) : (
+                <>
+                    <div className="dropdown">
+                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
+                            </svg>
+                        </label>
+                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box">
+                            {renderLinks()}
+                        </ul>
+                    </div>
+                    <div className="hidden lg:flex">
+                        <ul className="menu menu-horizontal p-0">
+                            {renderLinks()}
+                        </ul>
+                    </div>
+                </>
+            )}
         </div>
     )
 }
